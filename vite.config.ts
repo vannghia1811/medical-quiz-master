43f1960431b9550ddeb9bb1878a6d0f07132cf75
vannghia1811 <buivannghia1811@gmail.com>
2025-11-20 09:45:29 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/medical-quiz-master/",   // <<<<<<<<<<<<<< THÊM DÒNG NÀY
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  base: "/medical-quiz-master/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
